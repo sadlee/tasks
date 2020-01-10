58b239d5daded4ea6b557b6dfe47202ccab53e20
sadlee <sadlak.radoslaw@gmail.com>
2020-01-10 11:24:21 +0100

    initial commit

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
