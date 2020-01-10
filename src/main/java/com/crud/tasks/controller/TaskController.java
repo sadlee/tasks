f081f4f09e67506dead83160fc83455c801d1c3c
sadlee <sadlak.radoslaw@gmail.com>
2020-01-10 14:40:31 +0100

    apptest

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
