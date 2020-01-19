2b2a32e0ec44141ff0eebcf263e023da3e88ffdf
sadlee <sadlak.radoslaw@gmail.com>
2020-01-19 20:55:39 +0100

    19.4

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -1,7 +1,7 @@
 package com.crud.tasks.config;
 
 import lombok.Getter;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
