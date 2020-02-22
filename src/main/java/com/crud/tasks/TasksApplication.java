fc30f6a7d2ca977bc7ae24ea808c877a49ae14c4
sadlee <sadlak.radoslaw@gmail.com>
2020-02-22 23:36:57 +0100

    24.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,7 +2,7 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
 public class TasksApplication {
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
 
 @Service
 public class SimpleEmailService {
@@ -18,22 +19,38 @@ public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
+
     public void send(final Mail mail) {
         LOGGER.info("Starting email preparation...");
         try {
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent.");
         } catch (MailException e) {
             LOGGER.error("Failed to process email sending: ", e.getMessage(), e);
         }
     }
 
-    private SimpleMailMessage createMailMessage(final Mail mail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(mail.getMailTo());
-        Optional.ofNullable(mail.getToCc()).ifPresent(mailMessage::setCc);
-        mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-        return mailMessage;
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            if("Tasks: Once a day email".equals(mail.getSubject())) {
+                messageHelper.setText(mailCreatorService.buildTrelloTasksOneDayEmail(mail.getMessage()), true);
+            } else
+                messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
+
+//    private SimpleMailMessage createMailMessage(final Mail mail) {
+//        SimpleMailMessage mailMessage = new SimpleMailMessage();
+//        mailMessage.setTo(mail.getMailTo());
+//        Optional.ofNullable(mail.getToCc()).ifPresent(mailMessage::setCc);
+//        mailMessage.setSubject(mail.getSubject());
+//        mailMessage.setText(mail.getMessage());
+//        return mailMessage;
+//    }
 }
